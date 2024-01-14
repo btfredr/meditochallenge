e5677a84175caa731a8b2fb40d7cbd473358701d
Fredrik Fordelsen <fredrikf2000@gmail.com>
2024-01-14 16:00:11 +0100

    working on nav comp and added router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Donate from './components/Donate';
+import Nav from './components/Nav';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/donate" component={Donate} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const Nav = () => {
+  return (
+    <nav>
+    <NavLink to="/">Home</NavLink>
+    <NavLink to="/about">About</NavLink>
+    <NavLink to="/donate">Donate</NavLink>
+    {/* Add more links as needed */}
+  </nav>
+  )
+}
+
+export default Nav
